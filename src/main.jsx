eebde2f228a83d39cbdb4cd08f8339547fe2ac2a
asankaperera <asanka.wpp@gmail.com>
2025-08-03 23:25:53 -0600

    Add Okta authentication (signup or login)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{ 
+        redirect_uri: window.location.origin,
+        // audience: "https://dev-yohelt72e38t5w40.us.auth0.com/api/v2/",
+        // scope: "read:current_user update:current_user_metadata"
+       }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
-)
+);
